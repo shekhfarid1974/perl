8e699311efc4449911c76f5fc1168d72e86cea7d
shekhfarid1974 <shekhfarid1974@gmail.com>
2025-07-08 10:29:05 +0600

    learn pl

---
+++ learn.pl
@@ -0,0 +1,9 @@
+$employee_name = "John Doe";
+$employee_age = 30;
+$employee_department = "Engineering";
+$employee_salary = 7500.1;
+
+print "Employee Name: $employee_name\n";
+print "Employee Age: $employee_age\n";
+print "Employee Department: $employee_department\n";
+print "Employee Salary: $employee_salary\n";
